15d409791afa88869c25fe1214d3d7c6e7c5ed59
2hy <84903051+2hys@users.noreply.github.com>
2024-02-02 09:20:08 +0900

    [level 1] Title: 내적, Time: 0.16 ms, Memory: 31.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/70128. 내적/내적.cs
@@ -0,0 +1,14 @@
+using System;
+
+public class Solution {
+    public int solution(int[] a, int[] b) {
+        int answer = 0;
+        if (a.Length != b.Length)
+            return -1;
+        for (int i = 0;i < a.Length; i++)
+        {
+            answer += a[i]*b[i];
+        }
+        return answer;
+    }
+}
