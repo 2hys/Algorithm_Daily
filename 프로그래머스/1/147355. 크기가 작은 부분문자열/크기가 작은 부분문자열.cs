ef12df5a54365a9992a0269c02e3ebbf6e799d04
2hy <84903051+2hys@users.noreply.github.com>
2024-03-05 09:02:21 +0900

    [level 1] Title: 크기가 작은 부분문자열, Time: 0.79 ms, Memory: 31.5 MB -BaekjoonHub

--- 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.cs
+++ 프로그래머스/1/147355. 크기가 작은 부분문자열/크기가 작은 부분문자열.cs
@@ -4,8 +4,8 @@ public class Solution {
     public int solution(string t, string p) {
         int answer = 0;
         string tmp = "";
-        // if (t.Length <= p.Length)
-        //     return 0;
+        if (t.Length == p.Length)
+            return 1;
         for (int i = 0; i < t.Length - (p.Length - 1); i++)
         {
             for (int j = 0; j < p.Length; j++)
